b4552e51a9b4c660f024e07a5b936412c17ef2d4
Andrea <9698060@itisrossi.vi.it>
2023-02-11 10:54:59 +0100

    CodicePretto

---
+++ SITO3.js
@@ -0,0 +1,114 @@
+//funcction per il ccalcolo del controllo del codice "calcolaControllo"
+function calcolaControllo(risultato){
+    
+    for (let i = 0; i < risultato.length; i++) {
+        var posizioniRisultato = [];
+        let puntiRisultato = 0;
+        posizioniRisultato[i] = risultato.slice(i, i + 1);
+    }
+    for (let i = 0; i < array.length; i += 2) {//calcolo caratteri alfanumerici dispari
+        if(posizioniRisultato[i] == 0){//calcolo numeri
+            puntiRisultato = puntiRisultato + 1;//
+        }else{
+            if(posizioniRisultato[i] == 1){
+                puntiRisultato = puntiRisultato + 0;
+            }else{
+                if(posizioniRisultato[i] == 2){
+                    puntiRisultato = puntiRisultato + 5;
+                }else{
+                    if(posizioniRisultato[i] == 3){
+                        puntiRisultato = puntiRisultato + 7;
+                    }else{
+                        if(posizioniRisultato[i] == 4){
+                            puntiRisultato = puntiRisultato + 9;
+                        }else{
+                            if(posizioniRisultato[i] == 5){
+                                puntiRisultato = puntiRisultato + 13;
+                            }else{
+                                if(posizioniRisultato[i] == 6){
+                                    puntiRisultato = puntiRisultato + 15;
+                                }else{
+                                    if(posizioniRisultato[i] == 7){
+                                        puntiRisultato = puntiRisultato + 17;
+                                    }else{
+                                        if(posizioniRisultato == 8){
+                                            puntiRisultato = puntiRisultato + 19;
+                                        }else{
+                                            if(posizioniRisultato == 9){
+                                                puntiRisultato = puntiRisultato + 21;
+                                            }else{
+                                                if(posizioniRisultato == "A"){// calcolo lettere
+                                                    puntiRisultato = puntiRisultato + 1;
+                                                }else{
+                                                    if(posizioniRisultato == "B"){
+                                                        puntiRisultato = puntiRisultato + 0;
+                                                    }else{
+                                                        if(posizioniRisultato == "C"){
+                                                            puntiRisultato = puntiRisultato + 5;
+                                                        }else{
+                                                            if(posizioniRisultato == "D"){
+                                                                puntiRisultato = puntiRisultato + 7;
+                                                            }else{
+                                                                if(posizioniRisultato == "E"){
+                                                                    puntiRisultato = puntiRisultato + 9;
+                                                                }else{
+                                                                    if(posizioniRisultato == "F"){
+                                                                        puntiRisultato = puntiRisultato + 13;
+                                                                    }else{
+                                                                        if(posizioniRisultato == "G"){
+                                                                            puntiRisultato = puntiRisultato + 15;
+                                                                        }else{
+                                                                            if(posizioniRisultato == "H"){
+                                                                                puntiRisultato = puntiRisultato + 17;
+                                                                            }else{
+                                                                                if(posizioniRisultato == "I"){
+                                                                                    puntiRisultato = puntiRisultato + 19;
+                                                                                }else{
+                                                                                    if(posizioniRisultato == "J"){
+                                                                                        puntiRisultato = puntiRisultato + 21;
+                                                                                    }else{
+                                                                                        if(posizioniRisultato == "K"){
+                                                                                            puntiRisultato = puntiRisultato + 2;
+                                                                                        }else{
+                                                                                            if(posizioniRisultato == "L"){
+                                                                                                puntiRisultato = puntiRisultato + 4;
+                                                                                            }else{
+                                                                                                if(posizioniRisultato == "M"){
+                                                                                                    puntiRisultato = puntiRisultato + 18;
+                                                                                                }else;{
+                                                                                                    if(posizioniRisultato == "N"){
+                                                                                                        puntiRisultato = puntiRisultato + 20;
+                                                                                                    }
+                                                                                                }
+                                                                                            }
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                                
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    
+    }
+    for (let i = 1; i < array.lenght; i += 2){//calcolo cartteri alfanumerici pari
+        if(posizioeRisultato[i] == ){
+
+        }
+    }
+}
+    
