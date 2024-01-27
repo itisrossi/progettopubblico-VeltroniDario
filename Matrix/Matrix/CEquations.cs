f3da099cf15d6fb466e95be9820435589b3a6473
Veltroni Dario <9698074@itisrossi.vi.it>
2024-01-27 10:15:32 +0100

    Aggiunta files

---
+++ Matrix/Matrix/CEquations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    internal class CEquations
+    {
+        private int[] _a; 
+        private int[] _b;
+        private int[] _c;
+        private double _x, _y, _z;
+        private int _j, _k, _l;
+
+        public int[] a
+        {
+            get => _a;
+            set => _a = value;
+        }
+
+    }
+}
---
+++ Matrix/Matrix/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CMatrix b = new CMatrix(4);
+            CMatrix a = new CMatrix(5);
+
+            a.Refill(1, 3);
+
+            a.PrintMatrix();
+
+
+            Console.WriteLine("\n\n\n\nIl determinante è " + a.Determinante());
+            //b.Copia(a.Riduci(a.matrix[0, 0]));
+            //a.PrintMatrix();
+
+
+            Console.ReadLine();
+        }
+    }
+}
