04d3c2283373ff63962ff454eb5033cc2abbbb81
Dario <9698074@itisrossi.vi.it>
2024-02-15 09:07:59 +0100

    working list

---
+++ Matrix/Matrix/DoubleLinkedList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Matrix
+{
+    internal class DoubleLinkedList
+    {
+        internal DNode head;
+        internal void InsertFront(DoubleLinkedList doubleLinkedList, int data)
+        {
+            DNode newNode = new DNode(data);
+            newNode.next = doubleLinkedList.head;
+            newNode.prev = null;
+            if (doubleLinkedList.head != null)
+            {
+                doubleLinkedList.head.prev = newNode;
+            }
+            doubleLinkedList.head = newNode;
+        }
+
+        internal void InsertLast(DoubleLinkedList doubleLinkedList, int data)
+        {
+            DNode newNode = new DNode(data);
+            if (doubleLinkedList.head == null)
+            {
+                newNode.prev = null;
+                doubleLinkedList.head = newNode;
+                return;
+            }
+            DNode lastNode = GetLastNode(doubleLinkedList);
+            lastNode.next = newNode;
+            newNode.prev = lastNode;
+        }
+
+        internal DNode GetLastNode(DoubleLinkedList singlyList)
+        {
+            DNode temp = singlyList.head;
+            while (temp.next != null)
+            {
+               temp = temp.next;
+            }
+            return temp;
+        }
+    }
+    
+
+}
+
--- Matrix/Matrix/Program.cs
+++ Matrix/Matrix/Program.cs
@@ -7,14 +7,17 @@
 
          
             
+            DoubleLinkedList doubleLinkedList = new DoubleLinkedList();
+            doubleLinkedList.InsertFront(doubleLinkedList, 3);
 
+            /*
             CEquations e = new CEquations();
             e.Calculates();
 
             Console.WriteLine("x = " + e.x  + " y =  " + e.y + " z = " +  e.z);
-
+            */
 
             Console.ReadLine();
-        }
+        }         
     }
 }//LISTA:  aggiungi, togli, arrivare ad un posizione 
